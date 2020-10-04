ad8fd67a56f64248a013571b0e32265ab26a6524
Frank Eslami <eslamifrank@gmail.com>
2020-10-04 13:07:36 -0700

    Created Category model and updated the Product model.

---
+++ ProductShop2Tutorial/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductShop2Tutorial.Models {
+    public class Category {
+        public int CategoryID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a category name.")]
+        public string Name { get; set; }
+    }
+}
--- ProductShop2Tutorial/Models/Product.cs
+++ ProductShop2Tutorial/Models/Product.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProductShop2Tutorial.Models {
     public class Product {
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product price.")]
+        [Column(TypeName = "decimal(18, 2)")]   //18 length and 2 precision
         public decimal Price { get; set; }
-        public string Slug => Name.Replace(' ', '-');
+
+        [Required(ErrorMessage = "Please select a category.")]
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        public decimal DiscountPercent => 0.20M;
+        public decimal DiscountAmount => Price * DiscountPercent;
+        public decimal DiscountedPrice => Price - DiscountAmount;
+
+        [Required(ErrorMessage = "Please enter a product code.")]
+        public string Code { get; set; }
+
+        public string Slug => Name == null ? "" : Name.Replace(' ', '-');
     }
 }
