3b96958af4273dc6d5217b83841de18ea597be48
Frank Eslami <eslamifrank@gmail.com>
2020-10-06 04:28:18 -0700

    Details view created.

--- ProductShop2Tutorial/Controllers/CartController.cs
+++ ProductShop2Tutorial/Controllers/CartController.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using ProductShop2Tutorial.Models;
 
 namespace ProductShop2Tutorial.Controllers {
-    public class HomeController : Controller {
+    public class CartController : Controller {
         public IActionResult Index() {
             return View();
         }
 
-        [Route("[action]")]
-        public IActionResult About() {
+        public IActionResult Add(string productSlug) {
+            ViewBag.ProductSlug = productSlug;
             return View();
         }
     }
 }
- 
--- ProductShop2Tutorial/Controllers/ProductController.cs
+++ ProductShop2Tutorial/Controllers/ProductController.cs
@@ -7,12 +7,52 @@ using ProductShop2Tutorial.Models;
 
 namespace ProductShop2Tutorial.Controllers {
     public class ProductController : Controller {
-        public IActionResult Detail(string slug) {
-            return View();
+        private ShopContext shopContext;
+
+        public ProductController(ShopContext shopContext) {
+            this.shopContext = shopContext;
         }
 
-        public IActionResult List() {
-            return View();
+        public IActionResult Index() {
+            return RedirectToAction("List", "Product");     //Products is plural because the List action appends an s
+        }
+
+        public IActionResult Detail(int id) {
+            var categories = shopContext.Categories.OrderBy(c => c.CategoryID).ToList();
+            Product product = shopContext.Products.Find(id);
+            string imageFileName = product.Code + "-m.jpg";
+
+            //Get product's category
+            string categoryName = "";
+            foreach (var category in categories) {
+                if (category.CategoryID == product.CategoryID) {
+                    categoryName = category.Name;
+                    break;
+                }
+            }
+
+            ViewBag.CategoryName = categoryName;
+            ViewBag.ImageFileName = imageFileName;
+
+            return View(product);
+        }
+
+        [Route("[controller]s/{categoryName?}")]
+        public IActionResult List(string categoryName = null) {
+            var categories = shopContext.Categories.OrderBy(c => c.CategoryID).ToList();
+            List<Product> products;
+
+            if (categoryName == null) {
+                products = shopContext.Products.OrderBy(p => p.Name).ToList();
+            }
+            else {
+                products = shopContext.Products.Where(p => p.Name == categoryName).OrderBy(p => p.Name).ToList();
+            }
+
+            ViewBag.SelectedCategoryName = categoryName;
+            ViewBag.Categories = categories;
+
+            return View(products);
         }
     }
 }
