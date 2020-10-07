311cd05a30701c18dd119347cb6402bb69101863
Frank Eslami <eslamifrank@gmail.com>
2020-10-07 15:56:12 -0700

    AddUpdate works.

--- ProductShop2Tutorial/Areas/Admin/Controllers/CategoryController.cs
+++ ProductShop2Tutorial/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using ProductShop2Tutorial.Models;
 
 namespace ProductShop2Tutorial.Areas.Admin.Controllers {
@@ -22,6 +23,51 @@ namespace ProductShop2Tutorial.Areas.Admin.Controllers {
         public IActionResult List() {
             List<Category> categories = shopContext.Categories.OrderBy(c => c.Name).ToList();
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Add() {
+            ViewBag.Action = "Add";
+            return View("AddUpdate", new Category());
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id) {
+            ViewBag.Action = "Update";
+            Category category = shopContext.Categories.Find(id);
+            return View("AddUpdate", category);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Category category) {
+            if (ModelState.IsValid) {
+                if (category.CategoryID == 0) {
+                    shopContext.Categories.Add(category);
+                }
+                else {
+                    shopContext.Categories.Update(category);
+                }
+
+                shopContext.SaveChanges();
+                return RedirectToAction("List", "Category");
+            }
+            else {
+                ViewBag.Action = "Save";
+                return View("AddUpdate");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) {
+            Category category = shopContext.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category category) {
+            shopContext.Remove(category);
+            shopContext.SaveChanges();
+            return RedirectToAction("List", "Category");
         }
     }
 }
---
+++ ProductShop2Tutorial/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using ProductShop2Tutorial.Models;
+
+namespace ProductShop2Tutorial.Areas.Admin.Controllers {
+
+    [Area("Admin")]
+    public class ProductController : Controller {
+        private ShopContext shopContext;
+        private List<Category> categories;
+
+        public ProductController(ShopContext shopContext) {
+            this.shopContext = shopContext;
+            categories = shopContext.Categories.OrderBy(c => c.Name).ToList();
+        }
+
+        public IActionResult Index() {
+            return RedirectToAction("List", "Product");
+        }
+
+        [Route("[area]/[controller]s/{id?}")]
+        public IActionResult List(string id = "All") {
+            List<Product> products;
+            if (id == "All") {
+                products = shopContext.Products.OrderBy(p => p.Name).ToList();
+            }
+            else {
+                products = shopContext.Products.Where(p => p.Category.Name == id).OrderBy(p => p.Name).ToList();
+            }
+
+            ViewBag.Categories = categories;
+            ViewBag.SelectedCategoryName = id;
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add() {
+            Product product = new Product();
+            product.Category = shopContext.Categories.First();  //must set default category for FK contraint
+            
+            ViewBag.Action = "Add";
+            ViewBag.Categories = categories;
+
+            return View("AddUpdate", product);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id) {
+            Product product = shopContext.Products.Include(p => p.Category).FirstOrDefault(p => p.ProductID == id);
+
+            ViewBag.Action = "Update";
+            ViewBag.Categories = categories;
+
+            return View("AddUpdate", product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product) {
+            if (ModelState.IsValid) {
+                if (product.ProductID == 0) {
+                    shopContext.Products.Add(product);
+                }
+                else {
+                    shopContext.Products.Update(product);
+                }
+
+                shopContext.SaveChanges();
+                return RedirectToAction("List", "Product");
+            }
+            else {
+                ViewBag.Action = "Save";
+                ViewBag.Categories = categories;
+                return View("AddUpdate", product);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) {
+            Product product = shopContext.Products.FirstOrDefault(p => p.ProductID == id);  //same as Find(id)
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product) {
+            shopContext.Products.Remove(product);
+            shopContext.SaveChanges();
+
+            return RedirectToAction("List", "Product");
+        }
+    }
+}
